d28dfef27c460e0c6a7b0e930a90b1d13863ce70
rishabhvesit <2024.rishabhkumar.jain@ves.ac.in>
2025-04-25 02:56:03 +0530

    Update TestBase.java

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,7 +41,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "D:/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe"");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
